7107fdfeb62792d2ee18a1db1cd482df00e257cd
LorenzoBinosi <lorenzo.binosi@gmail.com>
2024-08-21 18:10:17 +0200

    Added -y on docker installation

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ echo \
 sudo apt-get update
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add user to docker group
 sudo usermod -aG docker $USER
